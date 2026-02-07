64a11a76f4eb06aabdacc6f28acb9d083d00d040
Angelo Lacson <angelo.lacson@irely.com>
2026-02-07 22:45:29 +0800

    place holder update

--- wedding-day/script.js
+++ wedding-day/script.js
@@ -30,3 +30,16 @@ window.onscroll = function() {
         nav.style.background = "rgba(255, 255, 255, 0.95)";
     }
 };
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(n => n.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+}));
